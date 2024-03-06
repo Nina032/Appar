4ced50797eb045adacd35e88e9dc2ba723e5bad1
Nevena Kicanovic <111436933+Nina032@users.noreply.github.com>
2024-03-06 12:39:53 +0100

    Lektion 24_03_06 med en hemside View

---
+++ Northwind.Mvc/Program.cs
@@ -0,0 +1,61 @@
+using Northwind.Shared;
+
+//Section 1 - import namespaces
+using Microsoft.AspNetCore.Identity; //IdentityUser
+using Microsoft.EntityFrameworkCore; //UseSqlServer, UseSqlite
+using Northwind.Mvc.Data;
+using Northwind.Common.DataContext.SqlServer;   //ApplicationDbContext
+
+//Section 2 - configure the host web server including services
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString)); //UseSqlite
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+string? sqlServerConnection = builder.Configuration
+    .GetConnectionString("NorthwindConnection");
+if (sqlServerConnection is null)
+{
+    Console.WriteLine("SQL Server database connection string is missing!");
+}
+else
+{
+    builder.Services.AddNorthwindContext(sqlServerConnection);
+}
+
+var app = builder.Build();
+
+// Section 3 - 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+//Section 4 - start the host web server listening for HTTP request
+app.Run(); //blocking call
